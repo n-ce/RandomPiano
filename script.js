22070c3fc30397b76214bc7ab49709f461d01d5a
n-ce <abrocks1120@gmail.com>
2021-09-11 16:52:50 +0530

    setInterval -> setTimeout

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 var count = 0;
 var body = document.body;
 body.onclick = function() {count++;}
-body.onload=function(){
-  setInterval(function(){
-    if (count % 2 == 1) {
-      body.style.backgroundImage = "url(Pause.webp)";
-      body.style.backgroundColor = "#059C6A";
-      sound = new Audio("Chords/"+Math.trunc(Math.random()*87)+".mp3");
-      sound.play();
-    }else{
-      body.style.backgroundImage = "url(Play.webp)";
-      body.style.backgroundColor = "#00D08C";
-      sound.pause();
-    }
-  },1000)
+
+setTimeout(timer,500);
+function command(x){
+  if(count % 2 == 1){
+    body.style.backgroundImage = "url(Pause.webp)";
+    body.style.backgroundColor = "#059C6A";
+    sound = new Audio("Chords/" + Math.trunc(Math.random() * 88) + ".mp3");
+    sound.play();
+  }
+  else {
+    body.style.backgroundImage = "url(Play.webp)";
+    body.style.backgroundColor = "#00D08C";
+    sound.pause();
+  }
+  setTimeout(x,Math.trunc(Math.random()*1000));
 }
+function timer(){command(timer2);}
+function timer2(){command(timer);}
