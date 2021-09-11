b4257ac7ae5a34439820190a5cdffb4c23ca9202
n-ce <abrocks1120@gmail.com>
2021-09-11 17:22:43 +0530

    settimeout test 2

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 var count = 0;
 var body = document.body;
+
 body.onclick = function() {count++;}
 
-setTimeout(timer,500);
+setTimeout(loop,500);
+
 function command(x){
+  var sound = new Audio("Chords/" + Math.trunc(Math.random() * 88) + ".mp3");
   if(count % 2 == 1){
     body.style.backgroundImage = "url(Pause.webp)";
     body.style.backgroundColor = "#059C6A";
-    sound = new Audio("Chords/" + Math.trunc(Math.random() * 88) + ".mp3");
     sound.play();
   }
-  else {
+  else{
     body.style.backgroundImage = "url(Play.webp)";
     body.style.backgroundColor = "#00D08C";
     sound.pause();
   }
   setTimeout(x,Math.trunc(Math.random()*1000));
 }
-function timer(){command(timer2);}
-function timer2(){command(timer);}
+function loop(){command(loop2);}
+function loop2(){command(loop);}
