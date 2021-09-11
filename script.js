fcc8dfcbe7e69d725e9a584e3abf9a04cb303eb9
n-ce <abrocks1120@gmail.com>
2021-09-11 13:42:10 +0530

    mp3 -> ogg

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ body.onload=function(){
   setInterval(function(){
     if (count % 2 == 1) {
       body.style.backgroundImage = "url(Pause.webp)";
-      sound = new Audio("Chords/. ("+Math.trunc(Math.random()*87)+").mp3");
+      sound = new Audio("Chords/. ("+Math.trunc(Math.random()*87)+").ogg");
       sound.play();
     }else{
       body.style.backgroundImage = "url(Play.webp)";
